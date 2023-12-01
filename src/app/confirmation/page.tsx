27cc9750cd0470f4848da0de4125fea09d51977f
Hilquias O. Rodrigues <hilquias.kil@gmail.com>
2023-12-01 20:31:26 -0300

    ADD: Confirmation page

---
+++ src/app/confirmation/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Confirmation() {
+  return (
+    <div className="p-8 lg:w-[340px] flex flex-col items-center lg:px-0 m-auto">
+      <Image
+        className="mb-4"
+        src="/confirmation.svg"
+        alt="confirmação"
+        width={60}
+        height={60}
+      />
+      <h2 className="text-xl text-[#191847] mb-3">Parabéns!</h2>
+      <h3 className="text-base text-[#C9C5D4] text-center mb-12">
+        Sua assinatura foi realizada <br /> com sucesso.
+      </h3>
+      <div className="p-4 rounded-2xl shadow-xl w-full mb-20">
+        <div className="bg-[#F4F3F6] p-4 flex items-center justify-between rounded-2xl mb-5">
+          <Image src="/success.svg" alt="sucesso" width={40} height={40} />
+          <div className="text-right">
+            <p className="text-[#191847] text-base mb-2">Anual <span className="mx-1">|</span> Parcelado</p>
+            <p className="text-[#191847] text-sm">R$ 479,90 <span className="mx-1">|</span> 10x R$ 47,99</p>
+          </div>
+        </div>
+        <p className="flex justify-between text-sm mb-6 px-5">
+          <span className="text-[#C9C5D4]">E-mail</span>
+          <span>fulano@cicrano.com.br</span>
+        </p>
+        <p className="flex justify-between text-sm px-5 mb-2">
+          <span className="text-[#C9C5D4]">CPF</span>
+          <span>000.000.000-00</span>
+        </p>
+      </div>
+      <Link href="/" className="text-[#191847] text-xs mb-6 font-bold">
+        Gerenciar assinatura
+      </Link>
+      <Link
+        href="/"
+        className="text-sm text-center font-bold text-white bg-[#191847] hover:bg-blue-950 p-4 rounded-3xl w-full transition-colors"
+      >
+        IR PARA A HOME
+      </Link>
+    </div>
+  );
+}
